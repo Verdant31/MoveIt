e0861aa4b2ed100019024c1d30178d0c6bbf64d5
Verdant31 <joao.pedro3110@hotmail.com>
2021-02-24 15:36:12 -0400

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,12 @@
+
+export function Profile() {
+    return (
+        <div>
+            <img src = "https://github.com/Verdant31.png" alt = "João Piovesan" />
+            <div>
+                <strong>João Piovesan</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
